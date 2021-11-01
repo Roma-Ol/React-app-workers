c722a25c89d88fcf4511aee73ed820b24d649734
RomaRoma <v12nes11@gmail.com>
2021-11-01 17:34:35 +0200

    tsk-2 added users using props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,17 @@
 import EmployeesListItem from "../employee-list-item/employee-list-item"
 import './employees-list.css'
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+  const elements = data.map(item => {
+    return (
+      <EmployeesListItem name={item.name} surname={item.surname} salary={item.salary} increase={item.increase}/>
+    )
+  })
+
   return(
     <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
+      {elements}
     </ul>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/app/app';
+ 
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
